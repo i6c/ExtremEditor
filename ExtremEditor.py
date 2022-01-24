91d83bdfc315134c07a85abaf28ae26dfd680097
i6c <87837042+i6c@users.noreply.github.com>
2022-01-24 07:40:30 +0000

    Update ExtremEditor.py
    Note, put this in its own directory for the mail sorter

--- ExtremEditor.py
+++ ExtremEditor.py
@@ -113,7 +113,7 @@ class Editor:
             print(f'{self.__ico}\n\n{fg("00FF00")} > {fg("00FF00")}Improved by i6c! Added multiple files support.\n')
             path = filedialog.askopenfilenames()
             for filename in path:
-                b = [b[b.index('@'):b.index(':')] for b in open(path, 'r', errors="ignore").read().split('\n') if "@" in b and ":" in b]
+                b = [b[b.index('@'):b.index(':')] for b in open(filename, 'r', errors="ignore").read().split('\n') if "@" in b and ":" in b]
             tempdic = {a: 0 for a in list(set(b))}
             for domain in b:
                 tempdic[domain] += 1
@@ -137,11 +137,11 @@ class Editor:
             choice = input()
 
             def CreateTexts(domain):
-                open(f"{path.split('/')[-1][:-4]}/{domain[0]}.txt", 'a').write("\n".join([a for a in open(path, 'r', errors="ignore").read().split('\n') if domain[0] in a]))
+                open(f'{filename.split("/")[-1][:-4]}_{domain[0]}.txt', 'a').write("\n".join([a for a in open(filename, 'r', errors="ignore").read().split('\n') if domain[0] in a]))
 
             if choice == "1":
                 try:
-                    os.mkdir(path.split('/')[-1][:-4])
+                    os.mkdir(filename.split('/')[-1][:-4])
                 except FileExistsError:
                     pass
                 for dom in domain[:49]:
